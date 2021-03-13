d2deea1fd6d99bee1dda563eb2864d08b63dd389
Nefreti <marzena.witekk@gmail.com>
2021-03-13 18:50:04 +0100

    home welcome componenet with video

---
+++ web-client/components/home-page/home-welcome.vue
@@ -0,0 +1,17 @@
+<template>
+  <video :src="video" style="max-height: 700px; height: 100vh; min-width: 100%; object-fit: cover" autoplay="true" loop="true"></video>
+</template>
+<script>
+
+export default {
+  name: "home-welcome",
+
+  data: () => ({
+    video: `${require('~/assets/images/blubki-video.mp4')}`
+  }),
+}
+</script>
+
+<style scoped>
+
+</style>
