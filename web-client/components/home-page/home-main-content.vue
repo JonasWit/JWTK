2f57891f9bc8c6e6a2baaa1c36d0a2c10fa478d9
Nefreti <marzena.witekk@gmail.com>
2021-03-15 23:15:42 +0100

    nothing imporant -cards

--- web-client/components/home-page/home-main-content.vue
+++ web-client/components/home-page/home-main-content.vue
@@ -2,10 +2,10 @@
 <v-container>
   <v-row class="d-flex justify-space-between" >
 
-    <v-card v-for="item in items" :key="item.id">
-      <v-img>{{ items.img }}</v-img>
-      <v-card-title>{{ items.title }} text here</v-card-title>
-      <v-card-text>{{ items.text }}</v-card-text>
+    <v-card v-for="item in items" :key="item.id" width="350px" class="mx-auto my-2">
+      <v-img :src="item.img" height="125" contain></v-img>
+      <v-card-title>{{ item.title }} text here</v-card-title>
+      <v-card-text>{{ item.text }}</v-card-text>
     </v-card>
 
   </v-row>
@@ -15,14 +15,37 @@
 <script>
 export default {
 name: "home-main-content",
-  data: () => ({
-    items: [
-      { id: 1, img:`${require('~/assets/images/home-about-1.png')}`, title: 'Kim jesteśmy?', text: 'WebCouple to projekt'},
-      { id: 2, img:`${require('~/assets/images/home-about-2.png')}`, title: 'Co robimy', text: 'Tworzymy strony internetowe i rozwiązania'},
-      { id: 3, img:`${require('~/assets/images/home-about-3.png')}`, title: 'Nasz cel', text: 'Skupiamy się na dostarczaniu usług dostosowanych'},
-    ]
-  })
-}
+  data: () => {
+    return ({
+      items: [
+        {
+          id: 1,
+          img: `${require('~/assets/images/home-about-1.png')}`,
+          title: 'Kim jesteśmy?',
+          text: 'WebCouple to projekt'
+        },
+        {
+          id: 2,
+          img: `${require('~/assets/images/home-about-2.png')}`,
+          title: 'Co robimy',
+          text: 'Tworzymy strony internetowe i rozwiązania'
+        },
+        {
+          id: 3,
+          img: `${require('~/assets/images/home-about-3.png')}`,
+          title: 'Nasz cel',
+          text: 'Skupiamy się na dostarczaniu usług dostosowanych'
+        },
+
+      ],
+
+    });
+     },
+  created() {
+    console.log(this.items)
+  },
+
+     }
 </script>
 
 <style scoped>
--- web-client/components/home-page/home-welcome.vue
+++ web-client/components/home-page/home-welcome.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
-  <video :src="video" style="max-height: 500px; height: 100vh; min-width: 100%; object-fit: cover" autoplay="true" loop="true"></video><div id="card" style="position:absolute; top: 10%; z-index: 999; ">
-        <v-card  elevation="23" outlined shaped tile style="max-width: 650px">
+    <video id="video" :src="video" style="max-height: 500px; height: 100vh; min-width: 100%; object-fit: cover"
+           autoplay="true" loop="true"></video>
+
+    <transition>
+      <div id="card" style="position:absolute; top: 10%; z-index: 999; ">
+        <v-card elevation="23" outlined shaped tile style="max-width: 650px">
           <v-card-title>Witaj na stronie Systemy Wspomagania Pracy</v-card-title>
           <v-card-text>Tworzymy strony internetowe i aplikacje webowe.
-          Do każdego zlecenia podchodzimy w sposób indywidualny skupiając się na potrzebach Klienta.</v-card-text>
+            Do każdego zlecenia podchodzimy w sposób indywidualny skupiając się na potrzebach Klienta.
+          </v-card-text>
           <v-card-subtitle>Zapraszamy do współpracy!</v-card-subtitle>
           <v-card-actions>
-            <v-btn class="my-3" >Sprawdź ofertę</v-btn>
+            <v-btn class="my-3">Sprawdź ofertę</v-btn>
           </v-card-actions>
-  </v-card>
-  </div>
+        </v-card>
+      </div>
+    </transition>
   </div>
 </template>
 <script>
@@ -27,19 +33,21 @@ export default {
 
 <style scoped>
 
-  #card {
-    animation: fadeInAnimation ease 10s;
-    animation-iteration-count: 1;
-    animation-fill-mode: forwards;
+#card {
+  animation: fadeInAnimation ease 10s;
+  animation-iteration-count: 1;
+  animation-fill-mode: forwards;
+}
+
+@keyframes fadeInAnimation {
+  0% {
+
+    opacity: 0;
   }
-  @keyframes fadeInAnimation {
-    0% {
-      opacity: 0;
-    }
-    100% {
-      opacity: 1;
-    }
+  100% {
+    opacity: 1;
   }
+}
 
 
 </style>
