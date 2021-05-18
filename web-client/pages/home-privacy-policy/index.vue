3a0be52bfec8ecb2c6f417eea10d6580e1f3e42e
Marzena <marzena.witekk@gmail.com>
2021-05-18 11:35:22 +0200

    client contacts page

--- web-client/pages/home-privacy-policy/index.vue
+++ web-client/pages/home-privacy-policy/index.vue
@@ -1,7 +1,137 @@
 <template>
-  <div>
-    <h1>Polityka prywatności będzie tutaj!</h1>
-  </div>
+  <v-container>
+    <h1>Polityka prywatności</h1>
+    <h2>Wstęp</h2>
+    <p>Prywatność Użytkowników korzystających z naszych usług jest dla nas bardzo ważna, dlatego dokładamy wszelkich
+      starań, aby należycie ją chronić i respektować prawa w tym zakresie. W tym celu za pomocą niniejszej polityki
+      prywatności opisujemy zasady przetwarzania przez nas Twoich danych
+      osobowych oraz wykorzystywania plików cookies na naszej stronie. Informujemy m.in. o tym jak i jakie dane
+      zbieramy,
+      jak je przetwarzamy i po co, kto jest ich administratorem, w
+      jaki sposób są zabezpieczone, czy je komuś udostępniamy oraz jakie masz prawa.</p>
+    <h2>Informacje ogólne</h2>
+    <ol type="1">
+      <li>
+        Administratorem Twoich danych osobowych i jednocześnie operatorem serwisu https://systemywp.pl, za którego
+        pośrednictwem zbierane są informacje i dane osobowe jest Marzena Witek prowadząca jednoosobową działalność
+        gospodarczą pod nazwą <span style="color: crimson ">Systemy Wspomagania Pracy, Kraków, NIP: 7343144682, REGON: 388896441</span>,
+        zwana dalej
+        Administratorem.
+      </li>
+      <li>
+        Administratorem w odniesieniu do danych osobowych klientów czy pracowników jest osoba korzystająca z serwisu
+        https://systemywp.pl . My zaś przetwarzamy te dane w Twoim imieniu wyłącznie na Twoje udokumentowane polecenie,
+        a bez takiego polecenia tylko w przypadku, gdyby zobowiązywało nas do tego prawo.
+      </li>
+      <li>
+        Inspektor danych osobowych - W przypadku wyznaczenia przez nas tego podmiotu, zostaniesz o tym poinformowany w
+        tym miejscu.
+      </li>
+      <li>
+        Kontakt z Administratorem Twoich danych osobowych i operatorem serwisu możliwy jest poprzez <span
+        style="color: crimson">e-mail: ________ lub
+        </span>korespondencyjnie na adres siedziby podany w pkt. 1 powyżej.
+      </li>
+      <li>
+        Dane osobowe zbierane przez Administratora za pośrednictwem serwisu są przetwarzane zgodnie z Rozporządzeniem
+        Parlamentu Europejskiego i Rady (UE) 2016/679 z dnia 27 kwietnia 2016 r. w sprawie ochrony osób fizycznych w
+        związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia
+        dyrektywy 95/46/WE (ogólne rozporządzenie o ochronie danych), zwane dalej RODO oraz ustawą o ochronie danych
+        osobowych z dnia 10 maja 2018r.
+      </li>
+      <li>
+        Jeśli zamierzasz założyć konto Użytkownika oraz korzystać z naszych usług, zostaniesz poproszony o podanie nam
+        Twoich danych osobowych.
+      </li>
+      <li>
+        Twoje dane przetwarzane są przez nas we wskazanych poniżej celach, związanych z funkcjonowaniem Serwisu i
+        świadczeniem usług w nim oferowanych. Usługi te mogą̨ polegać przede wszystkim na prowadzeniu Twojego konta i
+        udostępnianiu funkcji odpowiedniego dla potrzeb klienta oprogramowania dostępnego na naszym portalu.
+      </li>
+    </ol>
+
+    <h2>Cele przetwarzania danych osobowych</h2>
+    <ol type="1">
+      <li>
+        Nadrzędnym celem przetwarzania Twoich danych osobowych jest realizacja usług świadczonych drogą elektroniczną, w
+        zgodzie z postanowieniami regulaminu serwisu, a w szczególności świadczenie usług oferowanych w Serwisie.
+      </li>
+      <li>
+        <p>Szczegółowe cele przetwarzania to:</p>
+        <ol type="a">
+          <li>
+            Świadczenie usług oferowanych w serwisie– podstawa art. 6 ust. 1 lit. b i lit. f RODO,
+          </li>
+          <li>
+            Rejestrowanie konta i korzystanie z niego, aby zarządzać posiadanym kontem – podstawa art. 6 ust. 1 lit. b
+            RODO,
+          </li>
+          <li>
+            Kontaktowanie się w celu obsługi i świadczenia usług zgodnie z regulaminem – podstawa art. 6 ust. 1 lit. b i
+            lit. f RODO,
+          </li>
+          <li>
+            Rozpatrywanie ewentualnych reklamacji – podstawa art. 6 ust. 1 lit. b i lit. f RODO,
+          </li>
+          <li>
+            Poprawianie naszych usług i eliminowanie błędów w serwisie, które mogą się pojawić z przyczyn technicznych –
+            podstawa art. 6 ust. 1 lit. f RODO,
+          </li>
+          <li>
+            Zapewnienie bezpieczeństwa serwisu i oferowanych usług jak również przeciwdziałania oszustwom, nadużyciom i
+            łamaniu regulaminu – podstawa art. 6 ust. 1 lit. c i lit. f RODO,
+          </li>
+          <li>
+            Analizowanie ruchu na stronie, aby ulepszać nasze funkcjonalności – podstawa art. 6 ust. 1 lit. f RODO,
+          </li>
+          <li>
+            Prowadzenie ewidencji rachunkowych w celach podatkowo – księgowych – podstawa art. 6 ust. 1 lit. c i lit. f
+            RODO.
+          </li>
+        </ol>
+      </li>
+    </ol>
+    <h3>Podstawa przetwarzania danych:</h3>
+    <p>
+      W zależności od podejmowanych przez Ciebie czynności, mogą to być:
+    </p>
+    <ol type="a">
+      <li>umowa o świadczenie usług lub działania podejmowane na Twoje żądanie, zmierzające do jej zawarcia (art. 6 ust.
+        1 lit. b RODO),
+      </li>
+      <li>
+        ciążący na nas obowiązek prawny, np. związany z rachunkowością (art. 6 ust. 1 lit. c RODO),
+      </li>
+      <li>nasz prawnie uzasadniony interes, polegający na przetwarzaniu w celu ustalenia, dochodzenia lub obrony
+        ewentualnych roszczeń (art. 6 ust. 1 lit. f RODO),
+      </li>
+    </ol>
+    <h3>Podanie danych:</h3>
+    <ol type="a">
+      <li>
+        dobrowolne, ale w niektórych przypadkach może być konieczne do zawarcia umowy
+      </li>
+    </ol>
+    <h3>
+      Skutek niepodania danych:
+    </h3>
+    <p>W zależności od celu, w jakim dane są podawane:</p>
+    <ol type="a">
+      <li>
+        brak możliwości zarejestrowania się w Serwisie,
+      </li>
+      <li>
+        brak możliwości korzystania z usług Serwisu,
+      </li>
+    </ol>
+    <h3>Możliwość cofnięcia zgody: </h3>
+    <ol type="a">
+      <li>
+        w każdej chwili
+      </li>
+    </ol>
+    <p>Przetwarzanie danych do momentu cofnięcia przez Ciebie zgody pozostaje zgodne z prawem.</p>
+  </v-container>
 </template>
 
 <script>
--- web-client/pages/legal-application/clients/_client/contacts/index.vue
+++ web-client/pages/legal-application/clients/_client/contacts/index.vue
@@ -1,10 +1,36 @@
 <template>
-  <h1> LIst of contacts</h1>
+  <layout>
+    <h1> LIst of contacts</h1>
+    <v-card tile>
+      <v-list-item>
+
+      </v-list-item>
+    </v-card>
+  </layout>
 </template>
 
 <script>
+import Layout from "@/components/legal-app/layout";
+
 export default {
-  name: "index"
+  name: "index",
+  components: {Layout},
+  middleware: ['legal-app-permission', 'client', 'authenticated'],
+
+
+  data: () => ({
+    searchResult: "",
+    contactList: [],
+    contactSearchItems: [],
+    finished: false,
+    loading: false,
+
+  }),
+
+  async fetch() {
+    this.contactSearchItems = await this.$axios.$get(`/api/legal-app-client-contacts/client/${this.$route.params.client}/contacts`)
+    console.warn('clients contacts search results', this.contactSearchItems)
+  }
 }
 </script>
 
