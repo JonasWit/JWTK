886c1112f634e58cf2e0453d79b342a209b365f8
ViersJW <jonaszwitek@gmail.com>
2021-05-20 17:22:45 +0200

    contact update

--- SystemyWP.API/Projections/General/ContactProjections.cs
+++ SystemyWP.API/Projections/General/ContactProjections.cs
@@ -18,9 +18,31 @@ namespace SystemyWP.API.Projections.General
                 contactDetails.Title,
                 contactDetails.Name,
                 contactDetails.Surname,
-                contactDetails.Emails,
-                contactDetails.PhoneNumbers,
-                contactDetails.PhysicalAddresses,
+                Emails = contactDetails.Emails.AsQueryable().Select(x => new
+                {
+                    x.Comment,
+                    x.Email,
+                    x.CreatedBy,
+                    x.Id
+                }).ToList(),
+                PhoneNumbers = contactDetails.PhoneNumbers.AsQueryable().Select(x => new
+                {
+                    x.Id,
+                    x.Comment,
+                    x.CreatedBy,
+                    x.Number
+                }).ToList(),
+                PhysicalAddresses = contactDetails.PhysicalAddresses.AsQueryable().Select(x => new
+                {
+                    x.Id,
+                    x.Comment,
+                    x.CreatedBy,
+                    x.Building,
+                    x.City,
+                    x.Country,
+                    x.Street,
+                    x.PostCode
+                }).ToList(),
                 contactDetails.Comment
             };
         
