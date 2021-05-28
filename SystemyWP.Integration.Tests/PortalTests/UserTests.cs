8b6c0d27537ab7b7ba160fbf3047f7cca9d06475
ViersJW <jonaszwitek@gmail.com>
2021-05-28 13:45:06 +0200

    portal tests init

---
+++ SystemyWP.Integration.Tests/PortalTests/UserTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace SystemyWP.Integration.Tests.PortalTests
+{
+    public class UserTests: IClassFixture<AppInstance>
+    {
+        private readonly AppInstance _instance;
+
+        public UserTests(AppInstance instance)
+        {
+            _instance = instance;
+        }
+        
+        
+        
+        
+        
+        
+    }
+}
