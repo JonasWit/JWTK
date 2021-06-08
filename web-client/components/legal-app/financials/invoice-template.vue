a64f8f5f3e23b3ba4bcb9bc53ede85e49eee0636
Marzena <marzena.witekk@gmail.com>
2021-06-08 16:24:46 +0200

    Financials - html2pdf implementation

---
+++ web-client/components/legal-app/financials/invoice-template.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card>
+    <v-card-title>Invoice content here</v-card-title>
+    <v-col>
+      <v-list class="d-flex justify-space-between" v-for="item in selectedRecords" :key="item.id">
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-subtitle>Hours: {{ item.hours }}</v-list-item-subtitle>
+            <v-list-item-subtitle>Minutes: {{ item.minutes }}</v-list-item-subtitle>
+            <v-list-item-subtitle>Rate: {{ item.rate }}</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-col>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "invoice-template",
+  props: {
+    selectedRecords: {
+      required: true,
+      type: Array,
+      default: null
+    },
+  },
+
+}
+</script>
+
+<style scoped>
+
+</style>
