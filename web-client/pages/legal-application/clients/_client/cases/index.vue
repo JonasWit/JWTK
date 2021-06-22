731d2f1417f13e4386d9e791192af0a329c85bcc
Marzena <marzena.witekk@gmail.com>
2021-06-22 12:46:57 +0200

    cases 2

--- web-client/pages/legal-application/clients/_client/cases/index.vue
+++ web-client/pages/legal-application/clients/_client/cases/index.vue
@@ -2,132 +2,52 @@
   <layout>
     <template v-slot:content>
       <v-container>
-        <h1 class="my-2">
+        <v-card class="mx-auto" v-for="item in groupedCases" :key="item[0].group">
+          <v-card-title class="white--text orange darken-4">
+            {{ item[0].group }}
+            <v-spacer></v-spacer>
+            <v-btn color="white" class="text--primary" fab small>
+              <v-icon>mdi-plus</v-icon>
+            </v-btn>
+          </v-card-title>
+          <v-divider></v-divider>
+          <v-virtual-scroll height="300" :items="items" :item-height="50">
+            <template v-slot:default="{ item }">
+              <v-list-item>
+                <v-list-item-content v-for="object in item" :key="object.id">
+                  <v-list-item-title> {{ object.name }}</v-list-item-title>
+                </v-list-item-content>
+                <v-list-item-action>
+                  <v-btn depressed small>
+                    Przejdź do szczegółów
+                    <v-icon color="orange darken-4" right>
+                      mdi-open-in-new
+                    </v-icon>
+                  </v-btn>
+                </v-list-item-action>
+              </v-list-item>
+            </template>
+          </v-virtual-scroll>
+        </v-card>
+
+
+        <h1 class="my-5">
           Lista spraw
         </h1>
-        <v-expansion-panels class="expansion" focusable>
-          <v-expansion-panel v-for="item in groupedCases" :key="item[0].group">
-            <v-expansion-panel-header>
+        <v-row class="d-flex justify-space-around">
+          <v-card v-for="item in groupedCases" :key="item[0].group">
+            <v-card-title>
               {{ item[0].group }}
-            </v-expansion-panel-header>
-            <v-expansion-panel-content v-for="object in item" :key="object.id">
-              <v-toolbar flat color="primary" dark>
-                <v-toolbar-title>User Profile</v-toolbar-title>
-              </v-toolbar>
-              <v-tabs vertical>
-                <v-tab>
-                  <v-icon left>
-                    mdi-account
-                  </v-icon>
-                  Option 1
-                </v-tab>
-                <v-tab>
-                  <v-icon left>
-                    mdi-lock
-                  </v-icon>
-                  Option 2
-                </v-tab>
-                <v-tab>
-                  <v-icon left>
-                    mdi-access-point
-                  </v-icon>
-                  Option 3
-                </v-tab>
+            </v-card-title>
+            <v-card-subtitle v-for="object in item" :key="object.id">
+              {{ object.name }}
+            </v-card-subtitle>
 
-                <v-tab-item>
-                  <v-card flat>
-                    <v-card-text>
-                      <p>
-                        Sed aliquam ultrices mauris. Donec posuere vulputate arcu. Morbi ac felis. Etiam feugiat lorem
-                        non metus. Sed a libero.
-                      </p>
+          </v-card>
 
-                      <p>
-                        Nam ipsum risus, rutrum vitae, vestibulum eu, molestie vel, lacus. Aenean tellus metus, bibendum
-                        sed, posuere ac, mattis non, nunc. Aliquam lobortis. Aliquam lobortis. Suspendisse non nisl sit
-                        amet velit hendrerit rutrum.
-                      </p>
+        </v-row>
 
-                      <p class="mb-0">
-                        Phasellus dolor. Fusce neque. Fusce fermentum odio nec arcu. Pellentesque libero tortor,
-                        tincidunt et, tincidunt eget, semper nec, quam. Phasellus blandit leo ut odio.
-                      </p>
-                    </v-card-text>
-                  </v-card>
-                </v-tab-item>
-                <v-tab-item>
-                  <v-card flat>
-                    <v-card-text>
-                      <p>
-                        Morbi nec metus. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis
-                        tellus, vitae iaculis lacus elit id tortor. Sed mollis, eros et ultrices tempus, mauris ipsum
-                        aliquam libero, non adipiscing dolor urna a orci. Curabitur ligula sapien, tincidunt non,
-                        euismod vitae, posuere imperdiet, leo. Nunc sed turpis.
-                      </p>
 
-                      <p>
-                        Suspendisse feugiat. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis
-                        tellus, vitae iaculis lacus elit id tortor. Proin viverra, ligula sit amet ultrices semper,
-                        ligula arcu tristique sapien, a accumsan nisi mauris ac eros. In hac habitasse platea dictumst.
-                        Fusce ac felis sit amet ligula pharetra condimentum.
-                      </p>
-
-                      <p>
-                        Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a
-                        libero. Nam commodo suscipit quam. In consectetuer turpis ut velit. Sed cursus turpis vitae
-                        tortor. Aliquam eu nunc.
-                      </p>
-
-                      <p>
-                        Etiam ut purus mattis mauris sodales aliquam. Ut varius tincidunt libero. Aenean viverra rhoncus
-                        pede. Duis leo. Fusce fermentum odio nec arcu.
-                      </p>
-
-                      <p class="mb-0">
-                        Donec venenatis vulputate lorem. Aenean viverra rhoncus pede. In dui magna, posuere eget,
-                        vestibulum et, tempor auctor, justo. Fusce commodo aliquam arcu. Suspendisse enim turpis, dictum
-                        sed, iaculis a, condimentum nec, nisi.
-                      </p>
-                    </v-card-text>
-                  </v-card>
-                </v-tab-item>
-                <v-tab-item>
-                  <v-card flat>
-                    <v-card-text>
-                      <p>
-                        Fusce a quam. Phasellus nec sem in justo pellentesque facilisis. Nam eget dui. Proin viverra,
-                        ligula sit amet ultrices semper, ligula arcu tristique sapien, a accumsan nisi mauris ac eros.
-                        In dui magna, posuere eget, vestibulum et, tempor auctor, justo.
-                      </p>
-
-                      <p class="mb-0">
-                        Cras sagittis. Phasellus nec sem in justo pellentesque facilisis. Proin sapien ipsum, porta a,
-                        auctor quis, euismod ut, mi. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,
-                        sem. Nam at tortor in tellus interdum sagittis.
-                      </p>
-                    </v-card-text>
-                  </v-card>
-                </v-tab-item>
-              </v-tabs>
-
-              <v-list>
-                <v-list-item>
-                  <v-list-item-title>
-                    {{ object.name }}
-                  </v-list-item-title>
-                  <v-list-item-title>
-                    {{ object.signature }}
-                  </v-list-item-title>
-                  <v-list-item-subtitle>
-                    {{ object.id }}
-                  </v-list-item-subtitle>
-                </v-list-item>
-                <cases-notes :selected-case="object"/>
-              </v-list>
-
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-        </v-expansion-panels>
       </v-container>
     </template>
   </layout>
@@ -151,7 +71,7 @@ export default {
     signature: "",
     description: "",
     groupedCases: [],
-    tab: null,
+    benched: 0,
 
 
   }),
@@ -166,6 +86,16 @@ export default {
     }
 
   },
+
+  computed: {
+    items() {
+      return Array.from({length: this.length}, (k, v) => v + 1)
+    },
+    length() {
+      return 7000
+    },
+  },
+
   methods: {
     async searchListOfCases() {
       try {
@@ -185,7 +115,10 @@ export default {
       const groups = groupByKey(input, key)
       this.groupedCases = groups
       console.log('group by category fired', groups)
-    }
+    },
+    genRandomIndex(length) {
+      return Math.ceil(Math.random() * (length - 1))
+    },
 
   },
 
