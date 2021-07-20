22356123c2739f5412573cc0438fc2d28010d190
ViersJW <jonaszwitek@gmail.com>
2021-07-20 14:43:47 +0200

    update

--- SystemyWP.API/Forms/LegalApp/Reminders/Validation/ReminderFormValidation.cs
+++ SystemyWP.API/Forms/LegalApp/Reminders/Validation/ReminderFormValidation.cs
@@ -8,7 +8,6 @@ namespace SystemyWP.API.Forms.LegalApp.Reminders.Validation
         {
             RuleFor(x => x.Name).NotEmpty().MaximumLength(100);
             RuleFor(x => x.Message).NotEmpty().MaximumLength(200);
-            RuleFor(x => x.Public).NotEmpty();
             RuleFor(x => x.Start).NotEmpty();
             RuleFor(x => x.End).NotEmpty();
             RuleFor(x => x.ReminderCategory).NotNull().IsInEnum();
