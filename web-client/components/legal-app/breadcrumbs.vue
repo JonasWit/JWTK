c8242c0c41eebd65ad69ca752a38bbc01feb904a
ViersJW <jonaszwitek@gmail.com>
2021-08-19 09:03:44 +0200

    updates

--- web-client/components/legal-app/breadcrumbs.vue
+++ web-client/components/legal-app/breadcrumbs.vue
@@ -21,13 +21,13 @@ export default {
     crumbs: [],
   }),
   async fetch() {
-    let clientId = this.$route.params.client
+    let clientId = this.$route.params.client;
     let caseId = this.$route.params.case;
     if (clientId) {
-      await this.getClientData({clientId})
+      await this.getClientData({clientId});
     }
     if (caseId) {
-      await this.getCaseDetails({caseId})
+      await this.getCaseDetails({caseId});
     }
     this.crumbs = this.refreshCrumbs();
   },
@@ -37,32 +37,28 @@ export default {
   methods: {
     ...mapActions('legal-app-client-store', ['getClientData', 'getCaseDetails']),
     refreshCrumbs() {
-      const fullPath = this.$route.fullPath
-      const params = fullPath.substring(1).split('/')
-      const crumbs = []
-      let path = ''
+      const fullPath = this.$route.fullPath;
+      const params = fullPath.substring(1).split('/');
+      const crumbs = [];
+      let path = '';
 
       params.forEach((param, index, {length}) => {
-        path = `${path}/${param}`
-        console.log('path', path)
-        const match = this.$router.match(path)
+        path = `${path}/${param}`;
+        const match = this.$router.match(path);
         if (match.name !== 'index') {
-          console.log('match', match)
-          console.log('match name', match.name)
-
           if (index === length - 1) {
             crumbs.push({
               text: this.pathName(match.name),
               disabled: true,
-            })
+            });
           } else {
             crumbs.push(
               this.pathName(match.name, path)
-            )
+            );
           }
         }
-      })
-      return crumbs
+      });
+      return crumbs;
     },
     pathName(name, path) {
       let clientId = this.$route.params.client;
@@ -73,118 +69,118 @@ export default {
           text: this.clientDataFromFetch.name,
           disabled: true,
           href: path + '/',
-        }
+        };
       }
       if (name === 'legal-application') {
         return {
           text: 'Strona główna',
           disabled: false,
           href: path + '/',
-        }
+        };
       }
       if (name === 'legal-application-clients') {
         return {
           text: 'Lista Klientów',
           disabled: false,
           href: path + '/',
-        }
+        };
       }
       if (name === 'legal-application-clients-client-c-details') {
         return {
           text: 'Panel Klienta',
           disabled: false,
           href: path + '/',
-        }
+        };
       }
       if (name === 'legal-application-clients-client-contacts') {
         return {
           text: 'Kontakty',
           disabled: false,
           href: path + '/',
-        }
+        };
       }
       if (name === 'legal-application-clients-client-cases') {
         return {
           text: 'Sprawy',
           disabled: false,
           href: path + '/',
-        }
+        };
       }
       if (name === 'legal-application-clients-client-notes') {
         return {
           text: 'Notatki',
           disabled: false,
           href: path + '/',
-        }
+        };
       }
       if (name === 'legal-application-clients-client-financials') {
         return {
           text: 'Rozliczenia',
           disabled: false,
           href: path + '/',
-        }
+        };
       }
       if (name === 'legal-application-clients-client-archived-cases') {
         return {
           text: 'Archiwum spraw',
           disabled: false,
           href: path + '/',
-        }
+        };
       }
       if (name === 'legal-application-reminders') {
         return {
           text: 'Kalendarz',
           disabled: false,
           href: path + '/',
-        }
+        };
       }
       if (name === 'legal-application-archive') {
         return {
           text: 'Archiwum Klientów',
           disabled: false,
           href: path + '/',
-        }
+        };
       }
       if (path === `/legal-application/clients/${clientId}/cases/${caseId}` && name == null) {
         return {
           text: this.clientCaseDetails.name,
           disabled: true,
           href: path + '/',
-        }
+        };
       }
       if (name === 'legal-application-clients-client-cases-case-deadlines') {
         return {
           text: 'Terminy procesowe',
           disabled: false,
           href: path + '/',
-        }
+        };
       }
       if (name === 'legal-application-clients-client-cases-case-details') {
         return {
           text: 'Szczegóły sprawy',
           disabled: false,
           href: path + '/',
-        }
+        };
       }
       if (name === 'legal-application-clients-client-cases-case-notes') {
         return {
           text: 'Notatki',
           disabled: false,
           href: path + '/',
-        }
+        };
       }
       if (name === 'legal-application-clients-client-cases-case-accesses') {
         return {
           text: 'Dostępy',
           disabled: false,
           href: path + '/',
-        }
+        };
       }
 
     },
 
   }
-}
+};
 </script>
 
 <style scoped>
