c0b641e6e4b7fd33c3fd091e95601489ac7b565c
ViersJW <jonaszwitek@gmail.com>
2021-09-04 19:00:03 +0200

    updates

--- web-client/components/user-profile/remove-users.vue
+++ web-client/components/user-profile/remove-users.vue
@@ -7,23 +7,21 @@
       <v-col class="d-flex justify-center" cols="12">
         <div class="d-flex flex-column">
           <v-card-subtitle>
-            Możesz usunąć powiązanych użytkowników. Ta akcja jest nieodwracalna i spowoduje utratę dostępu do usług
-            związanych z portalem. W celu ponownego uzyskania dostępu konieczne będzie przesłanie prośby o utworzenie
-            nowego konta i otrzymanie nowego zaproszenia.
+            Możesz usunąć powiązanych użytkowników. Ta akcja jest nieodwracalna i spowoduje utratę dostępu do danych
+            kancekarii przez usuniętego użytjoiwnika. W celu ponownego uzyskania przez niego dostępu konieczne będzie
+            przesłanie prośby o ponowne dodanie.
           </v-card-subtitle>
           <v-divider></v-divider>
           <v-card-text v-if="relatedUsers.length > 0">
             <v-card-title>Lista powiązanych użytkowników</v-card-title>
 
             <v-chip v-for="(user, index) in relatedUsers" :key="user.id" class="ma-2" close color="red"
-                    text-color="white"
-                    @click:close="revokeAccess(user)">
+                    text-color="white" @click:close="revokeAccess(user)">
               {{ user.email }}
             </v-chip>
 
           </v-card-text>
-          <default-confirmation-dialog button-color="error"
-                                       v-on:action-confirmed="revokeAccess" title="Usuń dostęp"
+          <default-confirmation-dialog button-color="error" v-on:action-confirmed="revokeAccess" title="Usuń dostęp"
                                        button-text="Usuń Dostęp"
                                        message="Czy na pewno chcesz trwale usunąć powiązanego użytkownika?"
                                        tooltip-message="Usuń powiązanego użytkownika"/>
@@ -46,23 +44,24 @@ export default {
     ...mapState('auth', ['relatedUsers'])
   },
   methods: {
-    ...mapActions('auth', ['deleteAccount']),
+    ...mapActions('auth', ['reloadRelatedUsers']),
     async revokeAccess(user) {
+
       this.loader = true;
+
       const payload = {
         userId: user.id
-      }
-      console.log('User Id:', payload)
+      };
+      console.log('User Id:', payload);
       try {
-        await this.$axios.$post('/api/portal-admin/key-admin/legal-app/access-key/revoke}', payload);
+        await this.$axios.$post('/api/portal-admin/key-admin/legal-app/access-key/revoke', payload);
         this.$notifier.showSuccessMessage("Dostęp usunięty pomyślnie");
       } catch (error) {
         handleError(error);
       } finally {
-        setTimeout(() => {
-          this.$nuxt.refresh()
-          this.loader = false;
-        }, 1500)
+        this.reloadRelatedUsers();
+        this.$nuxt.refresh();
+        this.loader = false;
       }
     }
   }
