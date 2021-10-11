90f7154ab52fc700278f7d1629362eef29e160b7
ViersJW <jonaszwitek@gmail.com>
2021-10-11 12:57:06 +0200

    remove users updated

--- web-client/components/user-profile/remove-users.vue
+++ web-client/components/user-profile/remove-users.vue
@@ -16,13 +16,13 @@
             <v-card-title>Lista powiązanych użytkowników</v-card-title>
 
             <v-chip v-for="(user, index) in otherLegalRelatedUsers" :key="user.id" class="ma-2" close color="red"
-                    text-color="white" @click:close="revokeAccess(user)">
+                    text-color="white" @click:close="revokeLegalAccess(user)">
               {{ user.email }}
             </v-chip>
 
           </v-card-text>
-          <default-confirmation-dialog button-color="error" v-on:action-confirmed="revokeAccess" title="Usuń dostęp"
-                                       button-text="Usuń Dostęp"
+          <default-confirmation-dialog button-color="error" v-on:action-confirmed="revokeLegalAccess"
+                                       title="Usuń dostęp" button-text="Usuń Dostęp"
                                        message="Czy na pewno chcesz trwale usunąć powiązanego użytkownika?"
                                        tooltip-message="Usuń powiązanego użytkownika"/>
         </div>
@@ -45,7 +45,7 @@ export default {
   },
   methods: {
     ...mapActions('auth', ['reloadLegalAppRelatedUsers']),
-    async revokeAccess(user) {
+    async revokeLegalAccess(user) {
       const payload = {
         userId: user.id
       };
@@ -59,10 +59,39 @@ export default {
         this.reloadLegalAppRelatedUsers();
         this.$nuxt.refresh();
       }
+    },
+    async revokeMedicalAccess(user) {
+      const payload = {
+        userId: user.id
+      };
+      console.log('User Id:', payload);
+      try {
+        await this.$axios.$post('/api/portal-admin/key-admin/medical-app/access-key/revoke', payload);
+        this.$notifier.showSuccessMessage("Dostęp usunięty pomyślnie");
+      } catch (error) {
+        handleError(error);
+      } finally {
+        this.reloadLegalAppRelatedUsers();
+        this.$nuxt.refresh();
+      }
+    }
+  },
+  async revokeRestaurantAccess(user) {
+    const payload = {
+      userId: user.id
+    };
+    console.log('User Id:', payload);
+    try {
+      await this.$axios.$post('/api/portal-admin/key-admin/restaurant-app/access-key/revoke', payload);
+      this.$notifier.showSuccessMessage("Dostęp usunięty pomyślnie");
+    } catch (error) {
+      handleError(error);
+    } finally {
+      this.reloadLegalAppRelatedUsers();
+      this.$nuxt.refresh();
     }
   }
 };
-
 
 </script>
 
