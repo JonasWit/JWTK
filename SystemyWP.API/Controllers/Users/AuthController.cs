620cce49d9c69d437c2f2e95430dbcd1726953a5
ViersJW <jonasz.witek@gmail.com>
2021-12-25 12:37:27 +0100

    api login endpoint

--- SystemyWP.API/Controllers/Users/AuthController.cs
+++ SystemyWP.API/Controllers/Users/AuthController.cs
@@ -3,18 +3,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
+using SendGrid.Helpers.Errors.Model;
 using SystemyWP.API.Controllers.BaseClases;
+using SystemyWP.API.Forms.Admin;
 using SystemyWP.API.Services.Logging;
 using SystemyWP.API.Services.Storage;
 using SystemyWP.Data;
 using SystemyWP.Data.Enums;
+using SystemyWP.Data.Models.General;
 
 namespace SystemyWP.API.Controllers.Users
 {
-    [Route("api/auth")]
+    [Route("api/[controller]")]
     [Authorize]
     public class AuthController : ApiController
     {
@@ -34,6 +38,58 @@ namespace SystemyWP.API.Controllers.Users
             await portalLogger.Log(HttpContext.Request.Path.Value, user, LogType.Access, "Wylogowano",
                 "Logout Endpoint");
             return Redirect(env.IsDevelopment() ? "https://localhost:3000/" : "https://portal.systemywp.pl/");
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(
+            [FromBody] UserLoginForm form,
+            [FromServices] UserManager<IdentityUser> userManager,
+            [FromServices] PortalLogger portalLogger,
+            [FromServices] SignInManager<IdentityUser> signInManager,
+            [FromServices] IHostEnvironment env)
+        {
+            try
+            {
+                var returnUrl = env.IsDevelopment() ? @"https://localhost:3000/" : @"https://portal.systemywp.pl";
+                var signInResult = await signInManager
+                    .PasswordSignInAsync(form.Username, form.Password, true, lockoutOnFailure: true);
+
+                if (signInResult.Succeeded)
+                {
+                    var user = await userManager.FindByNameAsync(form.Username);
+                    await portalLogger.Log(HttpContext.Request.Path.Value, user, LogType.Access, "Logged in",
+                        "Auth endpoint");
+                    return Redirect(returnUrl);
+                }
+
+                if (signInResult.IsLockedOut)
+                {
+                    var user = await userManager.FindByNameAsync(form.Username);
+                    await portalLogger.Log(HttpContext.Request.Path.Value, user, LogType.Access, "Failed login attempt - account locked", "Auth endpoint");
+                    return StatusCode(StatusCodes.Status500InternalServerError, "Account locked");
+                }
+
+                if (!signInResult.Succeeded)
+                {
+                    await portalLogger
+                        .Log(new PortalLogRecord
+                        {
+                            Endpoint = HttpContext.Request.Path.Value,
+                            LogType = LogType.Access,
+                            Description = "Failed login attempt",
+                            CreatedBy = "Auth endpoint",
+                            UserEmail = "",
+                            UserName = form.Username,
+                            UserId = "",
+                        });
+                }
+                return NotFound();
+            }
+            catch (Exception e)
+            {
+                await HandleException(e);
+                return ServerError;
+            }
         }
 
         [HttpGet("delete")]
@@ -56,7 +112,7 @@ namespace SystemyWP.API.Controllers.Users
 
                 if (userProfile is not null) _context.Remove(userProfile);
                 await _context.SaveChangesAsync();
-                
+
                 var deleteResult = await userManager.DeleteAsync(user);
                 if (deleteResult.Succeeded)
                 {
