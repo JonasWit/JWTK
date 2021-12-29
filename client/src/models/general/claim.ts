fa2a7ba934a1eed131178784737c734833a51a65
ViersJW <jonasz.witek@gmail.com>
2021-12-29 19:53:03 +0100

    client deleted

--- client/src/models/general/claim.ts
+++
@@ -1,9 +0,0 @@
-class Claim {
-    claimType: string;
-    claimValue: string;
-
-    constructor(claimType: string, claimValue: string) {
-        this.claimType = claimType;
-        this.claimValue = claimValue;
-    }
-} 
