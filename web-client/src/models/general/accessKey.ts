03faead4534cdb6d3423b97419c665288fea248f
ViersJW <jonasz.witek@gmail.com>
2021-12-29 13:05:20 +0100

    t3

--- web-client/src/models/general/accessKey.ts
+++ web-client/src/models/general/accessKey.ts
@@ -1,10 +1,14 @@
 class AccessKey {
     id: number;
     name: string;
+    applicationType: string;
 
-    constructor(id: number, name: string) {
+    constructor(id: number, name: string, applicationType: string) {
         this.id = id;
         this.name = name;
         this.applicationType = applicationType;
     }
+    
+    
+    
 } 
