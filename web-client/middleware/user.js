94c81542f0e6ffde2b56a6c55c0011c24ba1c149
ViersJW <jonasz.witek@gmail.com>
2021-12-29 12:11:53 +0100

    old client removed

--- web-client/middleware/user.js
+++
@@ -1,5 +0,0 @@
-﻿export default function ({store, redirect}) {
-  if (!store.getters["auth/user"] && !store.getters["auth/userAdmin"] && !store.getters["auth/portalAdmin"]) {
-    redirect('/');
-  }
-}
