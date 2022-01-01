9f1163822759bd459daeb969820c7dd270a69bbb
ViersJW <jonasz.witek@gmail.com>
2022-01-01 16:50:24 +0100

    home updated

--- web-client/src/views/Home.vue
+++ web-client/src/views/Home.vue
@@ -1,6 +1,38 @@
 <template>
   <div>
     <p>Home page</p>
+    
+    <h4>Zobacz co mamy</h4>
+    
+    <div>
+      <div>
+        <img src="" alt="">
+        <div>
+          <span>Rozwiązania Indywidualne</span>
+          <span>Zaprojektujem i budujemy aplikacje</span>
+        </div>
+      </div>
+
+      <h4>Rzowiązania dla firm</h4>    
+      
+      <div>
+        
+        
+        
+        
+        
+        
+      </div>
+      
+      
+      
+      
+      
+    </div>
+    
+    
+    
+    
   </div>
 </template>
 
