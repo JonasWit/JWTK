bffa446ee9f9caafcb6ad88247bf8404797fe2a6
ViersJW <jonasz.witek@gmail.com>
2022-01-04 13:31:00 +0100

    test2

--- web-client/src/components/portal/user/Authorization.vue
+++ web-client/src/components/portal/user/Authorization.vue
@@ -1,9 +1,8 @@
 <template>
-<div class="flex justify-center md:justify-end">
-  <router-link :to="{ 'name': 'Login' }">Zaloguj</router-link>
-  <router-link class="ml-2" :to="{ 'name': 'Register' }">Zarejestruj</router-link>
-</div>
-  
+  <div class="flex justify-center md:justify-end">
+    <router-link :to="{ 'name': 'Login' }">Zaloguj</router-link>
+    <router-link class="ml-2" :to="{ 'name': 'Register' }">Zarejestruj</router-link>
+  </div>
 </template>
 
 <script>
