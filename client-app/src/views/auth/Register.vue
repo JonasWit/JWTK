82335d4d003da248475c5a3b55901b026d685e11
JonasWit <jonasz.witek@pm.me>
2022-04-23 16:50:31 +0200

    front end updates

--- client-app/src/views/auth/Register.vue
+++ client-app/src/views/auth/Register.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mt-8 mx-4">
+  <div class="mt-8">
     <div class="w-full p-3 m-auto bg-white border-t-4 border-blue-600 rounded shadow-lg shadow-purple-800/50 lg:max-w-md">
       <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div class="max-w-md w-full space-y-8">
