5ab4a44a1b7ac80c53fbdfcce3bdfbb052b6d6f7
JonasWit <jonasz.witek@pm.me>
2022-05-11 16:46:02 +0200

    logistics model added

--- SystemyWP.API.Logistics/Data/Models/Item.cs
+++ SystemyWP.API.Logistics/Data/Models/Item.cs
@@ -1,6 +1,11 @@
 namespace SystemyWP.API.Logistics.Data.Models;
 
-public class Item
+public record Item : BaseModel
 {
-    
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; }   = string.Empty;
+    public string ItemType { get; set; } = string.Empty;
+    public int Quantity { get; set; }
+    public DateTime Added { get; set; }
+    public DateTime Updated { get; set; }
 }
