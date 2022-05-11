12dc75cb26a3486aa4fc34fc59f82486d344de1e
JonasWit <jonasz.witek@pm.me>
2022-05-11 16:27:44 +0200

    logistics started

--- SystemyWP.API.Logistics/Data/Models/Item.cs
+++ SystemyWP.API.Logistics/Data/Models/Item.cs
@@ -1,11 +1,6 @@
 namespace SystemyWP.API.Logistics.Data.Models;
 
-public record Item : BaseModel
+public class Item
 {
-    public string Name { get; set; } = string.Empty;
-    public string Description { get; set; }   = string.Empty;
-    public string ItemType { get; set; } = string.Empty;
-    public int Quantity { get; set; }
-    public DateTime Added { get; set; }
-    public DateTime Updated { get; set; }
+    
 }
--- SystemyWP.API.Logistics/Services/ClusterServices.cs
+++ SystemyWP.API.Logistics/Services/ClusterServices.cs
@@ -2,7 +2,7 @@ namespace SystemyWP.API.Logistics.Services;
 
 public class ClusterServices
 {
-    public string LogisticsService { get; set; }  = string.Empty;
-    public string GastronomyService { get; set; }  = string.Empty;
-    public string MasterService { get; set; }  = string.Empty;
+    public string LogisticsService { get; set; } = "";
+    public string GastronomyService { get; set; } = "";
+    public string MasterService { get; set; } = "";
 }
