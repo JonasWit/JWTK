b31419d0e8e8432a57527e3df159b8c513e1f502
JonasWit <jonasz.witek@pm.me>
2022-06-25 11:47:23 +0200

    swagger added

--- SystemyWP.API/Program.cs
+++ SystemyWP.API/Program.cs
@@ -70,18 +70,18 @@ if (builder.Environment.IsDevelopment())
 {
     builder.Services.AddSwaggerGen(c =>
     {
-        c.SwaggerDoc("v1", new OpenApiInfo
-        {
-            Title = "systemywp",
-            Version = "v1"
-        });
+        c.SwaggerDoc("v1", new OpenApiInfo {Title = "systemywp", Version = "v1"});
         c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
         {
-            In = ParameterLocation.Header,
-            Description = "Please insert JWT with Bearer into field",
+            Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
             Name = "Authorization",
-            Type = SecuritySchemeType.ApiKey
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer"
         });
+
         c.AddSecurityRequirement(new OpenApiSecurityRequirement
         {
             {
@@ -91,9 +91,12 @@ if (builder.Environment.IsDevelopment())
                     {
                         Type = ReferenceType.SecurityScheme,
                         Id = "Bearer"
-                    }
+                    },
+                    Scheme = "oauth2",
+                    Name = "Bearer",
+                    In = ParameterLocation.Header
                 },
-                new string[] { }
+                new List<string>()
             }
         });
     });
