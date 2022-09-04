b9afbae42b5dd24e83f7a3d96b00569aeb04439d
JW <jonasz.witek@pm.me>
2022-09-04 17:06:32 +0200

    update

--- VappsMobile/AppConstants.cs
+++ VappsMobile/AppConstants.cs
@@ -2,6 +2,13 @@
 {
     internal static class AppConstants
     {
+        public struct AppColors
+        {
+            public const string Violet = "#3c2560";
+            public const string Gold = "#ffd606";
+            public const string Red = "#820828";
+        }
+
         public struct BaseUrls
         {
             public const string MasterUrl = "test";
