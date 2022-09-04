f8a7468ef6dfefe614d4dd3f061e305e3f70ae0b
JW <jonasz.witek@pm.me>
2022-09-04 15:21:04 +0200

    mobile-added

---
+++ VappsMobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace VappsMobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ VappsMobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace VappsMobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
