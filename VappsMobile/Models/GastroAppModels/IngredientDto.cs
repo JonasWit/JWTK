9cf57e6b3979ea0b3d9e9c013b49a06f349fe66c
JW <jonasz.witek@pm.me>
2022-10-02 12:33:35 +0200

    mud web added

--- VappsMobile/Models/GastroAppModels/IngredientDto.cs
+++ VappsMobile/Models/GastroAppModels/IngredientDto.cs
@@ -6,6 +6,7 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
+        public string Image { get; set; }
         public MeasurementUnits MeasurementUnits { get; set; }
         public int PricePerStack { get; set; }
         public double StackSize { get; set; }
