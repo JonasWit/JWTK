68c0abdacb242303f8f1d73420da91f1e49a53bf
JW <jonasz.witek@pm.me>
2022-10-22 13:27:30 +0200

    cleanup

--- SystemyWP.API.Logistics/Services/ClusterServices.cs
+++
@@ -1,8 +0,0 @@
-namespace SystemyWP.API.Logistics.Services;
-
-public class ClusterServices
-{
-    public string LogisticsService { get; set; }  = string.Empty;
-    public string GastronomyService { get; set; }  = string.Empty;
-    public string MasterService { get; set; }  = string.Empty;
-}
