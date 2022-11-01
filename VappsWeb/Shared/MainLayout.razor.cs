3235d42387083f0848da459641c40ee30bf9880b
JW <jonasz.witek@pm.me>
2022-11-01 10:17:50 +0100

    updates

--- VappsWeb/Shared/MainLayout.razor.cs
+++ VappsWeb/Shared/MainLayout.razor.cs
@@ -1,10 +1,20 @@
 using MudBlazor;
+using VappsWeb.Config;
 
 namespace VappsWeb.Shared
 {
     public partial class MainLayout
     {
-        private readonly MudTheme _theme = new();
+        private readonly MudTheme _theme = new()
+        {
+            Palette = new Palette()
+            {
+                Primary = AppConfig.Colors.Primary,
+                Secondary = AppConfig.Colors.Secondary,
+                AppbarBackground = AppConfig.Colors.Primary,
+            },
+        };
+
         private bool _isDarkMode;
         private bool _drawerOpen = true;
 
